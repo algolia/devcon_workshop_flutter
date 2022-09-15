311e66a784e7dcfb8cfb133318bf424d0c4bd35b
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-09-15 12:32:53 +0200

    implement home screen

--- lib/data/products_repository.dart
+++ lib/data/products_repository.dart
@@ -1,16 +1,31 @@
+import 'package:algolia_devcon_workshop/model/product.dart';
 import 'package:algolia_helper_flutter/algolia_helper_flutter.dart';
 
 import '../credentials.dart';
-import '../model/product.dart';
 import '../model/type_aliases.dart';
 
 class ProductsRepository {
-  // TODO(1.1): add `_shoesSearcher` searcher (query: shoes)
-  // TODO(1.2): add `_seasonalProductsSearcher` searcher (rules: 'home-spring-summer-2021')
+  final _shoesSearcher = HitsSearcher.create(
+    applicationID: Credentials.applicationID,
+    apiKey: Credentials.apiKey,
+    state: const SearchState(
+      indexName: Credentials.hitsIndex,
+      query: 'shoes',
+    ),
+  );
 
-  // TODO(1.3): get 'shoes' products results
-  ProductsStream getShoesProducts() => const Stream.empty();
+  final _seasonalProductsSearcher = HitsSearcher.create(
+    applicationID: Credentials.applicationID,
+    apiKey: Credentials.apiKey,
+    state: const SearchState(
+      indexName: Credentials.hitsIndex,
+      ruleContexts: ['home-spring-summer-2021'],
+    ),
+  );
 
-  // TODO(1.4): get filtered products results
-  ProductsStream getSeasonalProducts() => const Stream.empty();
+  ProductsStream getShoesProducts() => _shoesSearcher.responses
+      .map((response) => response.hits.map(Product.fromJson).toList());
+
+  ProductsStream getSeasonalProducts() => _seasonalProductsSearcher.responses
+      .map((response) => response.hits.map(Product.fromJson).toList());
 }
